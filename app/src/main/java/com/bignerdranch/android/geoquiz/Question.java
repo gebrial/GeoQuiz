62bd005d9957f44b9865a1c1be049bab5cd968a3
gebrial <gebrial@live.ca>
2015-08-06 19:28:34 -0700

    Add multiple questions/capability..

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
@@ -0,0 +1,30 @@
+package com.bignerdranch.android.geoquiz;
+
+/**
+ * Created by sanjaya on 06/08/15.
+ */
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue){
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
